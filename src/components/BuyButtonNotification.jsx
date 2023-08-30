80774ed571f4442875fd51c5acc7dd60abaedc27
Martin Fuks <martinfukss@gmail.com>
2023-08-30 18:23:55 -0300

    Mejora Mail Notification

--- src/components/BuyButtonNotification.jsx
+++ src/components/BuyButtonNotification.jsx
@@ -6,6 +6,20 @@ export const BuyButtonNotification = async (user,detailCarrito) => {
     const userName = user.nickname;
     const ordenCompra = detailCarrito
 
+    const htmlContent = `
+    <html>
+      <body style="text-align: center;">
+        <h1 style="color: black;">¡Gracias por tu compra, ${userName}!</h1>
+        <p style="color: black;">Te agradecemos por tu reciente compra en Strings And Keys. Debajo puedes ver los detalles de tu compra:</p>
+        <ul style="color: black; list-style: none">
+          ${ordenCompra.map((e) => `<li>- ${e.name}</li>`).join("")}
+        </ul>
+        <img src="https://res.cloudinary.com/dhit7strk/image/upload/v1693428861/MAIL_p2qzg7.png" alt="Imagen de agradecimiento" style="display: block; margin: 0 auto;">
+      </body>
+    </html>
+  `;
+  
+
     const response = await fetch(`${VITE_LOCAL_HOST}/email`, {
       method: "POST",
       headers: {
@@ -14,8 +28,9 @@ export const BuyButtonNotification = async (user,detailCarrito) => {
       body: JSON.stringify({
         userMail: userMail,
         titulo: `Gracias por tu compra ${userName}`,
-        contenido:
-          `¡Te agradecemos por tu reciente compra en Strings And Keys!, Debajo podras ver los detalles de tu compra: ${ordenCompra.map(e=>(e.name))}`,
+        // contenido:
+        //   `¡Te agradecemos por tu reciente compra en Strings And Keys!, Debajo podras ver los detalles de tu compra: ${ordenCompra.map(e=>(e.name))}`,
+        contenido: htmlContent,
       }),
     });
     if (response.ok) {
