11f50698750d31affab73db90db72257ce3851d9
VVIDALSI <106885575+VVIDALSI@users.noreply.github.com>
2023-09-05 20:59:37 -0500

    Se actualizan las graficas de estadisticas se borran console log y se agrega stock al detail

--- src/components/ChartStats.jsx
+++ src/components/ChartStats.jsx
@@ -18,6 +18,29 @@ import {
 } from "@chakra-ui/react";
 import { BarChart, CartesianGrid, ResponsiveContainer, XAxis, YAxis, Tooltip, Legend, Bar } from "recharts";
 
+const CustomTooltip = ({ active, label, payload }) => {
+  if (active) {
+    return (
+      <Box p={2} borderWidth="1px" borderColor="#ffa200" borderRadius="md" backgroundColor="black">
+        <p className="label">{`Mes: ${label}`}</p>
+        <p className="sales">{`Valor: ${payload[0].value.toLocaleString()}`}</p>
+      </Box>
+    );
+  }
+  return null;
+};
+
+const CustomYAxisTick = ({ x, y, payload }) => {
+  // Formatea el valor usando .toLocaleString()
+  const formattedValue = payload.value.toLocaleString();
+
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text x={0} y={0} dy={16} textAnchor="end" >{formattedValue}</text>
+    </g>
+  );
+};
+
 const ChartStats = (props) => {
   const { data, clase } = props;
 
@@ -25,12 +48,15 @@ const ChartStats = (props) => {
     <Box width="100%">
       <Flex justify="center">
         <ResponsiveContainer width={300} height="100%" aspect={1}>
-          <BarChart data={data} barSize={5}>
-            <Tooltip />
-            <CartesianGrid stroke="#FFFFFF" strokeDasharray="4"/>
+          <BarChart data={data} barSize={5} margin={{ left: 15 }}>
+            <Tooltip content={<CustomTooltip />} />
+            <CartesianGrid stroke="#FFFFFF" strokeDasharray="4" />
             <XAxis stroke="#FFFFFF" dataKey="month" />
-            <YAxis stroke="#FFFFFF" dataKey={clase} />
-
+            <YAxis
+              tickFormatter={(value) => value.toLocaleString()}
+              domain={[0, 'auto']} stroke="#FFFFFF"
+            />
+            
             <Bar dataKey={clase} fill="#ffa200" />
           </BarChart>
         </ResponsiveContainer>
