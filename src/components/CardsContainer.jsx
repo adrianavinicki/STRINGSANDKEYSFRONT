a9fa86e33bed06f0936e0212426fb21dfd9f75f3
Martin Fuks <martinfukss@gmail.com>
2023-09-08 16:29:20 -0300

    paginado, flechas no se ven; arreglado

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,7 +1,16 @@
 import ProductSimple from "../components/Card";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
-import { Box, Flex, Heading, Image, SimpleGrid, useColorModeValue, Text, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Image,
+  SimpleGrid,
+  useColorModeValue,
+  Text,
+  Button,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 //import {setPage} from "../redux/actions"
 
@@ -11,32 +20,34 @@ import {
   AlertIcon,
   AlertTitle,
   AlertDescription,
-} from '@chakra-ui/react'
-import { Search2Icon } from '@chakra-ui/icons'
+} from "@chakra-ui/react";
+import { Search2Icon } from "@chakra-ui/icons";
 import { setPage } from "../redux/actions";
 
 //alert//
 
 const CardsContainer = () => {
-
   const dispatch = useDispatch();
 
   const allproducts = useSelector((state) => state.products);
-  const filteredProducts = useSelector((state) => state.filteredProducts)
-  const filteredPage = useSelector((state) => state.currentPage)
+  const filteredProducts = useSelector((state) => state.filteredProducts);
+  const filteredPage = useSelector((state) => state.currentPage);
 
   const [currentPage, setCurrentPage] = useState(0);
   const productPerPage = 8;
-  const totalPages = filteredProducts ? Math.ceil(filteredProducts.length / productPerPage) : Math.ceil(allproducts.length / productPerPage);
+  const totalPages = filteredProducts
+    ? Math.ceil(filteredProducts.length / productPerPage)
+    : Math.ceil(allproducts.length / productPerPage);
 
-  const displayedProducts = filteredProducts ? filteredProducts.slice(
-    filteredPage * productPerPage,
-    (filteredPage + 1) * productPerPage
-  ) :
-    allproducts.slice(
-      filteredPage * productPerPage,
-      (filteredPage + 1) * productPerPage
-    );
+  const displayedProducts = filteredProducts
+    ? filteredProducts.slice(
+        filteredPage * productPerPage,
+        (filteredPage + 1) * productPerPage
+      )
+    : allproducts.slice(
+        filteredPage * productPerPage,
+        (filteredPage + 1) * productPerPage
+      );
 
   const handleNext = () => {
     if (filteredPage < totalPages - 1) {
@@ -55,18 +66,19 @@ const CardsContainer = () => {
       <Box>
         <Flex
           justify="center"
-          color={useColorModeValue('black', 'white')}
+          color={useColorModeValue("black", "white")}
           alignItems="center"
         >
           <Button
             width={5}
             height={5}
-            backgroundColor={"black"}
+            backgroundColor={useColorModeValue("black", "white")}
+            color={useColorModeValue("white", "black")}
             onClick={handlePrevious}
             disabled={filteredPage === 0}
             _hover={{
-              transform: 'translateY(-2px)', // Aumenta el tamaño al pasar el mouse
-              cursor: 'pointer', // Cambia el cursor al puntero al pasar el mouse sobre el botón
+              transform: "translateY(-2px)", // Aumenta el tamaño al pasar el mouse
+              cursor: "pointer", // Cambia el cursor al puntero al pasar el mouse sobre el botón
             }}
           >
             ←
@@ -75,14 +87,15 @@ const CardsContainer = () => {
             Página {filteredPage + 1} de {totalPages}
           </Text>
           <Button
+            color={useColorModeValue("white", "black")}
             width={5}
             height={5}
-            backgroundColor={"black"}
+            backgroundColor={useColorModeValue("black", "white")}
             onClick={handleNext}
             disabled={filteredPage === totalPages - 1}
             _hover={{
-              transform: 'translateY(-2px)', // Aumenta el tamaño al pasar el mouse
-              cursor: 'pointer', // Cambia el cursor al puntero al pasar el mouse sobre el botón
+              transform: "translateY(-2px)", // Aumenta el tamaño al pasar el mouse
+              cursor: "pointer", // Cambia el cursor al puntero al pasar el mouse sobre el botón
             }}
           >
             →
@@ -92,51 +105,62 @@ const CardsContainer = () => {
 
       <Box>
         <Flex>
-          <SimpleGrid columns={4} bg={""} w={"100%"} h={"100%"} >
-            {
-              displayedProducts?.length ? (
-                displayedProducts.map((product) => {
-                  return (
-                    <ProductSimple
-                      key={product.id}
-                      id={product.id}
-                      image={product.image}
-                      name={product.name}
-                      brand={product.brand}
-                      price={product.price}
-                      productoInfo={product}
+          <SimpleGrid columns={4} bg={""} w={"100%"} h={"100%"}>
+            {displayedProducts?.length ? (
+              displayedProducts.map((product) => {
+                return (
+                  <ProductSimple
+                    key={product.id}
+                    id={product.id}
+                    image={product.image}
+                    name={product.name}
+                    brand={product.brand}
+                    price={product.price}
+                    productoInfo={product}
+                  />
+                );
+              })
+            ) : (
+              <>
+                {/* Espacio en blanco para la primera columna */}
+                <Box gridColumn="1" />
+                {/* Alert centrado en las columnas 2 y 3 */}
+                <Box gridColumn="span 2" mt={"15vh"}>
+                  <Alert
+                    status="success"
+                    variant="subtle"
+                    flexDirection="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    textAlign="center"
+                    borderRadius="10px"
+                  >
+                    <Search2Icon
+                      color={useColorModeValue("black", "white")}
+                      boxSize="5vh"
+                      mr={0}
                     />
-                  );
-                })
-              ) : (
-                <>
-                  {/* Espacio en blanco para la primera columna */}
-                  <Box gridColumn="1" />
-                  {/* Alert centrado en las columnas 2 y 3 */}
-                  <Box gridColumn="span 2" mt={'15vh'}>
-                    <Alert
-                      status="success"
-                      variant="subtle"
-                      flexDirection="column"
-                      alignItems="center"
-                      justifyContent="center"
-                      textAlign="center"
-                      borderRadius="10px"
+                    <AlertTitle
+                      color={useColorModeValue("black", "white")}
+                      mt={4}
+                      mb={1}
+                      fontSize="3.5vh"
                     >
-                      <Search2Icon color={useColorModeValue("black", "white")} boxSize="5vh" mr={0} />
-                      <AlertTitle color={useColorModeValue("black", "white")} mt={4} mb={1} fontSize="3.5vh">
-                        Sin Resultados!
-                      </AlertTitle>
-                      <AlertDescription color={useColorModeValue("black", "white")} maxWidth="sm" fontSize="2vh">
-                        No hay productos que coincidan con tu búsqueda.
-                      </AlertDescription>
-                    </Alert>
-                  </Box>
-                  {/* Espacio en blanco para la cuarta columna */}
-                  <Box gridColumn="span 1" />
-                </>
-              )
-            }
+                      Sin Resultados!
+                    </AlertTitle>
+                    <AlertDescription
+                      color={useColorModeValue("black", "white")}
+                      maxWidth="sm"
+                      fontSize="2vh"
+                    >
+                      No hay productos que coincidan con tu búsqueda.
+                    </AlertDescription>
+                  </Alert>
+                </Box>
+                {/* Espacio en blanco para la cuarta columna */}
+                <Box gridColumn="span 1" />
+              </>
+            )}
           </SimpleGrid>
         </Flex>
       </Box>
