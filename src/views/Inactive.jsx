3c981bb7c6920500867e7faf72029050e75390bb
Martin Fuks <martinfukss@gmail.com>
2023-09-08 00:39:13 -0300

    BAN Mejora

--- src/views/Inactive.jsx
+++ src/views/Inactive.jsx
@@ -1,56 +1,77 @@
 import WithSubnavigation from "../components/NavBar";
 import SmallWithLogoLeft from "../components/Footer";
-import { Box, Flex, Text, useColorModeValue, Icon } from "@chakra-ui/react";
-import { NotAllowedIcon } from '@chakra-ui/icons'
+import { Box, Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import { NotAllowedIcon } from "@chakra-ui/icons";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
+import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa"
 
 const Inactive = () => {
-
-    const usuarioActual = useSelector((state)=> state.actualUser)
+  const usuarioActual = useSelector((state) => state.actualUser);
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    if(usuarioActual.user_status === true){
-      navigate("/")
-    };
-  },[])
+  useEffect(() => {
+    if (usuarioActual.user_status === true) {
+      navigate("/");
+    }
+  }, []);
 
-    return(
-        <Box
-        backgroundImage={useColorModeValue("url('/bg.jpg')", "url('/bgdark.jpg')")}
-        backgroundPosition="center"
-        backgroundRepeat="no-repeat"
-        backgroundSize="cover"
-        w={"100%"}
-          h={"100vh"}
-        overflow={"hidden"}>
-            <Flex direction={"column"}>
-            <WithSubnavigation></WithSubnavigation>
-            <Box
-              color={"black"}
-              bg={useColorModeValue('rgb(0,0,0,0.4)', 'rgb(0,0,0,0.7)')}
-              w={"100%"}
-              h={"92vh"}
-              >
-            <Flex direction={"column"} align={"center"}>
-                <Icon><NotAllowedIcon w={"50vh"}></NotAllowedIcon></Icon>
-            <Text pt={"27vh"} w={"100vh"} fontSize={"5vh"} fontWeight={"bold"}>Lo siento, esta cuenta ha sido bloqueada por motivos administrativos.
-            <br />
-            Para más información comunícate con nosotros en nuestro canales digitales:
-            <br/> 
-            <br/> 
-            <Link to="https://instagram.com/stringsandkeyss?utm_source=qr&igshid=MzNlNGNkZWQ4Mg==" target="_blank">Instagram</Link> -
-            <Link to="https://twitter.com/StringsAndKeyss" target="_blank">Twitter</Link> -
-            <Link to="https://www.youtube.com/channel/UCBrqSo2as6a07D0sTKEBUdw" target="_blank">YouTube</Link> ...
-            </Text>
-            </Flex>
-            </Box>
-            <SmallWithLogoLeft></SmallWithLogoLeft>
-            </Flex>
+  return (
+    <Box>
+      <Flex direction={"column"}>
+        <Box>
+          <WithSubnavigation></WithSubnavigation>
         </Box>
-    )
+        <Box
+          backgroundImage={useColorModeValue(
+            "url('/bg.jpg')",
+            "url('/bgdark.jpg')"
+          )}
+          backgroundPosition="center"
+          backgroundRepeat="no-repeat"
+          backgroundSize="cover"
+          w={"100%"}
+          h={"83vh"}
+          mt={"10vh"}
+          pt={"2vh"}
+          overflow={"hidden"}
+        >
+          <Flex direction={"column"} align={"center"} h={'70vh'} bg={''} justify={'center'}>
+            <NotAllowedIcon color={"#ffa200"} boxSize={'25vh'}></NotAllowedIcon>
+            <Text textAlign={'center'} fontSize={"3vh"} fontWeight={"bold"}>
+              Lo siento, esta cuenta ha sido bloqueada por motivos
+              administrativos.
+              <br />
+              Para más información comunícate con nosotros en nuestro canales
+              digitales:
+              <br />
+            </Text>
+            <Flex mt={'5vh'} justify={'space-between'} bg={''} w={'50vh'}>
+              <Link
+                to="https://instagram.com/stringsandkeyss?utm_source=qr&igshid=MzNlNGNkZWQ4Mg=="
+                target="_blank"
+              >
+                <FaInstagram size={'10vh'} ></FaInstagram>
+              </Link>{" "}
+              <Link to="https://twitter.com/StringsAndKeyss" target="_blank">
+                <FaTwitter size={'10vh'}></FaTwitter>
+              </Link>{" "}
+              <Link
+                to="https://www.youtube.com/channel/UCBrqSo2as6a07D0sTKEBUdw"
+                target="_blank"
+              >
+                <FaYoutube size={'10vh'}></FaYoutube>
+              </Link>{" "}
+            </Flex>
+          </Flex>
+        </Box>
+        <Box marginTop="auto">
+          <SmallWithLogoLeft></SmallWithLogoLeft>
+        </Box>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Inactive;
