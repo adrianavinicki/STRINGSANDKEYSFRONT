57e784c006fb24f049d34f7aa03ef84b7f3e6d3a
Martin Fuks <martinfukss@gmail.com>
2023-09-08 19:35:47 -0300

    /products bug

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -8,7 +8,7 @@ import { getProducts } from "../redux/actions";
 import React from "react";
 import CardsContainer from "../components/CardsContainer";
 import FilterAndOrder from "../components/Filters";
-import { setPage } from "../redux/actions";
+import { setPage, filterCategory } from "../redux/actions";
 
 const Products = () => {
   const dispatch = useDispatch();
